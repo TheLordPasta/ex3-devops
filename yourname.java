27f7cb174ac20d74e449e3b463442b1ec9c8ce00
TheLordPasta <arielshirkanistudent@gmail.com>
2024-05-20 14:33:53 +0300

    first commit

---
+++ yourname.java
@@ -0,0 +1,10 @@
+public class yourname
+{
+	public static void main(String[] args){
+        String arielTxt = "ariel";
+		for(int i=1;i<=10;i++){
+			System.out.println(arielTxt);
+            arielTxt = arielTxt + " ariel";
+		}
+	}
+}
